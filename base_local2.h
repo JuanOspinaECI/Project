05a8a221f43551a7638e4e0ef7075457fd4bdf6b
JuanOspinaECI <juan.ospina-s@mail.escuelaing.edu.co>
2021-05-05 20:29:59 -0500

    scheisse base v 2.0

---
+++ base_local2.h
@@ -0,0 +1,36 @@
+#ifndef BASE_LOCAL_H
+#define BASE_LOCAL_H
+
+#include <string>
+#include <sqlite3.h>
+
+
+using namespace std;
+
+struct Dato
+{
+    double temp, lat, lon, alt, preci, vel, dir;
+    int hum;
+    string tipo;
+    Dato() {temp = lat = lon = alt = vel = dir = preci = 0.0; hum = 0;}
+};
+
+class DB_Local{
+
+public:
+    DB_Local(string path);
+    bool AbrirDB();
+    bool CerrarDB();
+    bool GuardarDatoProm(Dato a);
+    bool GuardarDatoMax(Dato a);
+    bool GuardarDatoMin(Dato a);
+    bool ValidarPass(string username, string passwd);
+    bool GuardarHist(string us, int hora, int min, string fecha);
+	bool GuardarUser(std::string us, string nom, string ape, string doc, string fecha , string pass);
+
+private:
+    string  _path;
+    sqlite3 *_db;
+};
+
+#endif // BASE_LOCAL_H
